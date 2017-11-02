64875ae905d6fbcc0d0d4098aed802fe9a9d3fa8
MariyamA <MariyamA@D12000664.namindia.com>
2017-11-02 12:59:34 +0530

    java commit to master

---
+++ spark_demo/src/spark_demo/sparkDemo.java
@@ -0,0 +1,10 @@
+package spark_demo;
+
+public class sparkDemo {
+	
+	public static void main(String args[]){
+		System.out.println("Git is Simple");
+	    System.out.println("Git is SCM");
+	    System.out.println("Git is DVCS");
+	}    
+}
